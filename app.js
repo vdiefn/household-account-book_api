6a639d0a0debda0aace8958a4c99d468d7dd2212
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-08-11 10:18:52 +0800

    feat:add cors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
+
 const port = process.env.port || 3000
+
+app.use(cors())
+app.use(express.json())
 
 app.get("/", (req, res)=> {
   res.send("Hello from backend api!")
