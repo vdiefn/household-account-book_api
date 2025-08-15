3195510aa56f6b5635f18a10543fae39edcb9f72
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-08-15 10:19:53 +0800

    refactor: mongoose connection refactor

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
-const mongoose = require("mongoose")
 const Record = require("./models/record")
-
-if(process.env.NODE_ENV !== "production") {
-    require("dotenv").config()
-}
-
-mongoose.connect(process.env.MONGODB_URI)
+require("./config/mongoose")
 
 const port = process.env.port || 3000
-const db = mongoose.connection
-
-db.on("error", () => {
-  console.log("mongodb error!")
-})
-
-db.once("open", () => {
-  console.log("mongodb connected")
-})
 
 app.use(cors())
 app.use(express.json())
