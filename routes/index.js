d8f5af3c20efa89785b5cf8c2accba5043c08368
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-08-29 16:00:35 +0800

    feat: add user router

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const record = require("./modules/record")
 const category = require("./modules/category")
 const summary = require("./modules/summary")
+const user = require("./modules/user")
 
 router.use("/records", record)
 router.use("/categories", category)
 router.use("/summary", summary)
+router.use("/user", user)
 
 module.exports = router
