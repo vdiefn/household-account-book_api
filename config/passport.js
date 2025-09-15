a11bd52c9434166df69377b9550031f654782d8e
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-09-15 14:38:23 +0800

    feat: update passport with JWT Strategy

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,11 @@
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
+const passportJWT = require('passport-jwt')
 const bcrypt = require("bcryptjs")
 const User = require("../models/user")
+
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
 
 module.exports = app => {
   app.use(passport.initialize())
@@ -11,7 +15,7 @@ module.exports = app => {
     done(null, user.id)
   })
 
-  passport .deserializeUser((user, done) => {
+  passport.deserializeUser((id, done) => {
     User.findById(id)
     .lean()
     .then(user => done(null, user))
@@ -33,5 +37,20 @@ module.exports = app => {
         })
       })
       .catch(err => done(err, false))
+  }))
+
+  const jwtOption = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+  }
+
+  passport.use(new JWTStrategy(jwtOption, async(jwtPayload, done) => {
+    try {
+        const user = await User.findById(jwtPayload._id).lean();
+        if (user) return done(null, user);
+        return done(null, false);
+      } catch (err) {
+        return done(err, false);
+      }
   }))
 }
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const record = require("./modules/record")
 const category = require("./modules/category")
 const summary = require("./modules/summary")
 const user = require("./modules/user")
+const passport = require("passport")
 
-router.use("/records", record)
-router.use("/categories", category)
-router.use("/summary", summary)
+router.use("/records", passport.authenticate("jwt", { session: false }), record)
+router.use("/categories", passport.authenticate("jwt", { session: false }), category)
+router.use("/summary", passport.authenticate("jwt", { session: false }), summary)
 router.use("/users", user)
 
 module.exports = router
